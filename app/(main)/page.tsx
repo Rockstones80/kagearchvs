78d5021c84ba18d77ed85b064dc15e116cd40b0a
uzor samuel <samueluzor80@gmail.com>
2025-11-15 21:00:51 +0100

    fix: resolve an issue in the main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Navbar from "@/components/landing/navbar";
 import Hero from "@/components/landing/hero";
-import Collection from "@/components/landing/collection";
-import Hero2 from "@/components/landing/hero2";
 import BlogSection from "@/components/landing/blog-section";
 import Footer from "@/components/landing/footer";
 
